d5646cdf64ace9dad0678c12ba09dd13687782fc
Sergey Pozhilov <pozhilov@gmail.com>
2022-04-27 21:07:17 +0300

    LinkButton component

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-link :to="to" class="py-3 px-[60px] rounded font-bold bg-black text-white hover:bg-blue-700">
+    {{ text }}
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'LinkButton',
+  props: {
+    to: {
+      type: String,
+      default: ""
+    },
+    text: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
