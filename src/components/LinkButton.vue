92023e07580df9e209f502760c5f86c004d4d9b1
Sergey Pozhilov <pozhilov@gmail.com>
2022-04-27 23:48:33 +0300

    Add "isDisabled" state to the button comp

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,21 +1,46 @@
 <template>
-  <router-link :to="to" class="py-3 px-[60px] rounded font-bold bg-black text-white hover:bg-blue-700">
+  <span>
+    <span
+      v-if="isDisabled"
+      class="py-3 px-[60px] rounded font-bold border-2"
+      :class="kind === 'primary' ? 'border-black bg-black text-white opacity-40' : 'border-gray-300 text-black opacity-40'"
+    >
+      {{ text }}
+    </span>
+    <router-link
+      v-else
+      :to="to"
+      class="py-3 px-[60px] rounded font-bold border-2"
+      :class="kind === 'primary'
+         ? 'border-black bg-black text-white hover:bg-blue-700 hover:border-blue-700'
+         : 'border-gray-300 text-black hover:border-gray-700'"
+    >
     {{ text }}
-  </router-link>
+    </router-link>
+  </span>
 </template>
 
 <script>
 export default {
   name: 'LinkButton',
+
   props: {
     to: {
       type: String,
       default: ""
     },
+    kind: {
+      type: String,
+      default: "primary"
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
     text: {
       type: String,
       default: ""
     }
-  }
+  },
 };
 </script>
